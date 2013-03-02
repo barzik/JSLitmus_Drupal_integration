798531a6ace1efc707f1620871fde48de2870452
Ran Bar-Zik <ran@bar-zik.com>
2013-03-02 18:42:56 +0200

    First commit

---
+++ jslitmus_integration/js/vendor/test.js
@@ -0,0 +1,32 @@
+/**
+ * Looping through all of Drupal.behaviours
+**/
+
+for (var behavior in Drupal.behaviors) {
+		 runJSLitmusText(behavior);	
+}
+
+/**
+ * running the JSLitmus tests.
+**/
+
+function runJSLitmusText(behavior) {
+	var behavior = 'Drupal.behaviors.'+behavior;
+	JSLitmus.test(behavior, function() {
+		executeFunctionByName(behavior+'.attach', window, arguments);
+	});
+}
+	
+/**
+ * Running the namespace behavior without eval
+**/
+	
+function executeFunctionByName(functionName, context /*, args */) {
+  var args = Array.prototype.slice.call(arguments).splice(2);
+  var namespaces = functionName.split(".");
+  var func = namespaces.pop();
+  for(var i = 0; i < namespaces.length; i++) {
+    context = context[namespaces[i]];
+  }
+  return context[func].apply(this, args);
+}
